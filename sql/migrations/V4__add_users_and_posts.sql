2c97fdd217fd5af90dfbc9f7ab5583aee59be323
apartapatia <raupov.deal@yandex.ru>
2026-02-09 21:13:01 +0300

    add new migrations

---
+++ sql/migrations/V4__add_users_and_posts.sql
@@ -0,0 +1,12 @@
+WITH company_users AS (
+    INSERT INTO users (name)
+    VALUES
+        ('admin'),
+        ('developer')
+    RETURNING id
+)
+INSERT INTO posts (owner_id, text)
+SELECT 
+    id,
+    "Тестовый пост"
+FROM company_users;
